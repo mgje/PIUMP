a9dcaf385304f4c48877a21af490e8f5448848ba
mgje <martin.guggisberg@gmail.com>
2018-10-17 15:24:29 +0200

    ueb 9

--- hs2018/ueb_K2_08.py
+++ hs2018/ueb_K2_08.py
@@ -1,17 +1,29 @@
-#Uebeung 2.8 x-Koordinaten
+#Uebung 2.9 Zahlenmauer
 
-x = 1.0
-e = 2.0
-dx = 0.05
-li = []
+l = [4,2,8,9,1,3,4]
+mauer = []
 
-while x <= e:
-    li.append(x)
-    x = x + dx
-    # Rundungsfehler vermeiden
-    # nach jeder Erhöhung auf 3 Stellen runden
-    z = x *1000
-    x = round(z)/ 1000 
+# Zahlenmauer berechnen
+while len(l) > 0:
+    mauer.append(l)
+    nl = []                 # Beginn eine neue Liste
+    for i in range(len(l)): # Wiederhole so oft, wie die Liste l lang ist
+        if i < len(l)-1:    # Falls nicht der letzte Durchlauf
+            nl.append(l[i]+l[i+1]) # Hänge die Summe der beiden Elemente i und i+1 an die Liste nl
+    l = nl                  # Nun ist die nächste Reihe fertig gestellt.
+    
 
 
-print(li)
+# Zahlenmauer ausgeben
+mauer.reverse() #Drehe die Reihenfolge die Mauer um
+tabs = 24       # Leerzeichen für die oberste Reihe
+for l in mauer:
+    s = '' + tabs*'  '  
+    for z in l:
+        s = s + ' %3d       '%z
+    print s
+    tabs = tabs - 3 # 3 Leerzeichen weniger für die nächste Reihe
+    
+        
+    
+
---
+++ hs2018/ueb_K2_9.py
@@ -0,0 +1,11 @@
+#Uebung 2.9 Summe
+
+l = [1,3,5,-5,7,23,-13,-33,27.5]
+
+s = 0
+for z in l:
+    s = s + z
+    
+print(s)
+print(sum(l))
+    
