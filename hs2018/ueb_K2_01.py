d47e22fd93447bd9aa85e1999ded0354b1824323
mgje <martin.guggisberg@gmail.com>
2019-08-16 08:50:45 +0200

    neues semster

--- hs2018/ueb_K2_01.py
+++
@@ -1,8 +0,0 @@
-print ('------------------')
-C = -20                 # Startwert von C
-dC = 5                  # Erhöhung pro Durchlauf
-while C <= 40:          # Schleifenkopf
-    F = (9.0/5)*C + 32  # 1. Anweisung in der Schleife
-    print (C, F)          # 2. Anweisung in der Schleife
-    C = C + dC          # 3. Anweisung in der Schleife
-print ('------------------')
--- hs2018/ueb_K3_02.py
+++
@@ -1,6 +0,0 @@
-#uebung 3.2
-
-def sn(N):
-    return sum([1/x**2 for x in range(1,N+1)])
-
-print sn(400)
