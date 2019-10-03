fe7fb3c9ef497b1168d33906172761baba213a35
mgje <martin.guggisberg@gmail.com>
2019-10-03 09:57:00 +0200

    42 als cube

--- hs2019/ueb18.py
+++ hs2019/ueb18.py
@@ -1,12 +1,17 @@
 #ueb 18
 from cmath import *
 a = complex(-1,0)
+print("Eingabe: a=%.2f +%.2fi"%(a.real,a.imag))
 w = phase(a)
-l = abs(a)
-print(l,w)
-
+r = abs(a)
+print("Polar: r= %.2f , w=%.2f"%(r,w))
 #Ueberprüfung
-z = l* exp(complex(0,w))
-print(z)
+z = r* exp(complex(0,w))
+print("polar->kartestisch: z=%.2f +%.2fi"%(z.real,z.imag))
+
+
+
+
+print("\n\n")
 print(a)
 print(z-a)
